4b454f2dc7e40a515fb6ccd1045014e2185393a3
= <=>
2017-01-23 00:15:41 +0800

    test

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from Tools import DocManagerTool
 from Tools import SampleTool
+from Tools import ShellManagerTool
 
 # DocManagerTool.DocManager_main.main()
-SampleTool.SampleTool_main.main()
+#SampleTool.SampleTool_main.main()
+# ShellManagerTool.SM_main.main()
