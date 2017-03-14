764f7d3dabea4d0aaf7f66ac908b60a5684e3513
Prototype <263693992@qq.com>
2017-03-14 17:50:12 +0800

    add pyqtterm widget

--- Tools/SampleTool_m/MainDocWidget.py
+++ Tools/SampleTool_m/MainDocWidget.py
@@ -2,11 +2,12 @@
 
 import sys
 import os.path as path
+from functools import partial
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 import numpy as np
 import SampleWidget as sw
-import CopyWidget as cw
+import Tools.pyqtterm as pyqtterm
 
 QtCore.QTextCodec.setCodecForTr(QtCore.QTextCodec.codecForName("utf8"))
 
@@ -14,8 +15,12 @@ QtCore.QTextCodec.setCodecForTr(QtCore.QTextCodec.codecForName("utf8"))
 class MainDockWidget(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(MainDockWidget, self).__init__(parent)
+        self.__setupVariables()
         self.__setupUI()
         self.show()
+
+    def __setupVariables(self):
+        self.dockWidList = []
 
     def __setupUI(self):
         self.setWindowTitle(self.tr("依靠窗口"))
@@ -23,6 +28,7 @@ class MainDockWidget(QtGui.QMainWindow):
         # 主窗口(采样窗口), SampleWidget对象
         self.sampleWidget = sw.SampleWidget()
         self.setCentralWidget(self.sampleWidget)
+        self.dockWidList.append(["central", self.sampleWidget])
 
         # # 停靠窗口2
         # dock2 = QtGui.QDockWidget(self.tr("停靠窗口2"), self)
@@ -41,9 +47,28 @@ class MainDockWidget(QtGui.QMainWindow):
 
         self.__setUpMenubar()
 
+        self.resizeMainWid()
+
+    def resizeMainWid(self):
         ## 调整窗口大小
-        centralWidSize = self.centralWidget().size()
-        self.resize(centralWidSize.width(), centralWidSize.height())
+        mainWidSize = QtCore.QSize(0, 0)
+        for (key, obj) in self.dockWidList:
+            if key == "bottom":
+                # width = mainWidSize.width() if mainWidSize.width() >= obj.width() else mainWidSize.width()+obj.width()
+                height = mainWidSize.height()+obj.height()
+                mainWidSize.setWidth(width)
+                mainWidSize.setHeight(height)
+            elif key == "central":
+                width = 600
+                height = 600
+                mainWidSize.setWidth(width)
+                mainWidSize.setHeight(height)
+            elif key == "right":
+                width = mainWidSize.width() + obj.width()
+                # height = mainWidSize.height() if mainWidSize.height() >= obj.height() else mainWidSize.height()+obj.height()
+                mainWidSize.setWidth(width)
+                mainWidSize.setHeight(height)
+        self.resize(mainWidSize)
 
     def __setUpMenubar(self):
         action_exit = QtGui.QAction(QtGui.QIcon(), '&exit', self)
@@ -71,9 +96,14 @@ class MainDockWidget(QtGui.QMainWindow):
         action_Start2EndArray.setShortcut('Ctrl+2')
         action_Start2EndArray.triggered.connect(self.sampleWidget.On_Action_Start2EndArray)
 
-        action_dockwid_tools = QtGui.QAction(QtGui.QIcon(), '&show window', self)
+        action_dockwid_tools = QtGui.QAction(QtGui.QIcon(), '&tools window', self)
         # action_window.setShortcut('Ctrl+2')
-        action_dockwid_tools.triggered.connect(self.On_Action_ShowDockWid)
+        action_dockwid_tools.triggered.connect(self.On_Action_ShowDockTools)
+
+        action_dockwid_terminal = QtGui.QAction(QtGui.QIcon(), '&terminal', self)
+        # action_window.setShortcut('Ctrl+2')
+        # action_dockwid_terminal.triggered.connect(self.On_Action_ShowDockWid)
+        action_dockwid_terminal.triggered.connect(self.On_Action_ShowDockTernimal)
 
         action_startlabel = QtGui.QAction(QtGui.QIcon(), '&start label', self)
         # action_window.setShortcut('Ctrl+2')
@@ -92,6 +122,7 @@ class MainDockWidget(QtGui.QMainWindow):
 
         windowMenu = menubar.addMenu('&window')
         windowMenu.addAction(action_dockwid_tools)
+        windowMenu.addAction(action_dockwid_terminal)
 
         toolsMenu = menubar.addMenu('&tools')
         toolsMenu.addAction(action_screenShot_cp)
@@ -99,6 +130,18 @@ class MainDockWidget(QtGui.QMainWindow):
         toolsMenu.addAction(action_Start2EndArray)
         toolsMenu.addAction(action_startlabel)
         toolsMenu.addAction(action_savelabel)
+
+    def __setTerminal(self):
+        ## 停靠窗口: terminal
+        self.dock_term = QtGui.QDockWidget(self.tr("Terminal"), self)
+        self.dock_term.setFeatures(QtGui.QDockWidget.DockWidgetFloatable | QtGui.QDockWidget.DockWidgetClosable)
+        term_wid = pyqtterm.TerminalWidget(self)
+        term_wid.setMinimumSize(800, 200)
+        self.dock_term.setWidget(term_wid)
+        self.addDockWidget(QtCore.Qt.BottomDockWidgetArea, self.dock_term)
+        self.dockWidList.append(["bottom", self.dock_term])
+
+        self.resizeMainWid()
     
     def __setUpDockTools(self):
         """
@@ -115,19 +158,24 @@ class MainDockWidget(QtGui.QMainWindow):
         button_ok.setGeometry(101, 0, 40, 20)
         self.dock_tools.setWidget(self.tools_wid)
         self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.dock_tools)
+        self.dockWidList.append(["right", self.dock_tools])
 
         button_ok.clicked.connect(self.On_button_ok)
 
-        # 调整窗口大小
-        centralWidSize = self.centralWidget().size()
-        dock_toolsSize = self.dock_tools.size()
-        self.resize(dock_toolsSize.width() + centralWidSize.width(), centralWidSize.height())
+        self.resizeMainWid()
 
-    def On_Action_ShowDockWid(self, event):
+    def On_Action_ShowDockTools(self, event):
         try:
             self.dock_tools.show()
         except AttributeError, e:
             self.__setUpDockTools()
+
+    def On_Action_ShowDockTernimal(self, event):
+        try:
+            self.dock_term.show()
+        except AttributeError, e:
+            self.__setTerminal()
+
 
     def On_button_ok(self, event):
         if self.sampleWidget.filePathsList.count() == 0:
